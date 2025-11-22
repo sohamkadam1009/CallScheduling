b2c51bd8358f927ab4f368566a0d8fedab429ac9
soham <sohamkadam16102004@gmail.com>
2025-11-22 11:39:19 +0530

    Done with animation screen

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,55 +1,76 @@
+// import React from "react";
+// import styles from "./LoadingPage.module.css";
+// import Abhishek from "../assets/abhishek.webp";
+// import Pooja from "../assets/pooja.webp";
+// import Varun from "../assets/varun.webp";
+
+// const LoadingPage = () => {
+//   return (
+//     <div classNameName={styles["loading-page"]}>
+//       <div classNameName={styles.container}>
+//         <div classNameName={styles["profiles-container"]}>
+//           <div
+//             classNameName={`${styles["profile-wrapper"]} ${styles["profile-1"]}`}
+//           >
+//             <img
+//               src={Pooja}
+//               alt="Investment Expert 1"
+//               classNameName={styles["profile-pic"]}
+//             />
+//           </div>
+
+//           <div
+//             classNameName={`${styles["profile-wrapper"]} ${styles["profile-2"]}`}
+//           >
+//             <img
+//               src={Abhishek}
+//               alt="Investment Expert 2"
+//               classNameName={`${styles["profile-pic"]} ${styles["profile-pic-main"]}`}
+//             />
+//           </div>
+
+//           <div
+//             classNameName={`${styles["profile-wrapper"]} ${styles["profile-3"]}`}
+//           >
+//             <img
+//               src={Varun}
+//               alt="Investment Expert 3"
+//               classNameName={styles["profile-pic"]}
+//             />
+//           </div>
+//         </div>
+
+//         <h1 classNameName={styles.title}>Congratulations!</h1>
+
+//         <p classNameName={styles.subtitle}>
+//           your meeting has been scheduled with one of your experts from Investza
+//           <span classNameName={styles["loading-dots"]}>
+//             <span>.</span>
+//             <span>.</span>
+//             <span>.</span>
+//           </span>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+
 import React from "react";
 import styles from "./LoadingPage.module.css";
-import Abhishek from "../assets/abhishek.webp";
-import Pooja from "../assets/pooja.webp";
-import Varun from "../assets/varun.webp";
 
 const LoadingPage = () => {
   return (
-    <div className={styles["loading-page"]}>
-      <div className={styles.container}>
-        <div className={styles["profiles-container"]}>
-          <div
-            className={`${styles["profile-wrapper"]} ${styles["profile-1"]}`}
-          >
-            <img
-              src={Pooja}
-              alt="Investment Expert 1"
-              className={styles["profile-pic"]}
-            />
-          </div>
-
-          <div
-            className={`${styles["profile-wrapper"]} ${styles["profile-2"]}`}
-          >
-            <img
-              src={Abhishek}
-              alt="Investment Expert 2"
-              className={`${styles["profile-pic"]} ${styles["profile-pic-main"]}`}
-            />
-          </div>
-
-          <div
-            className={`${styles["profile-wrapper"]} ${styles["profile-3"]}`}
-          >
-            <img
-              src={Varun}
-              alt="Investment Expert 3"
-              className={styles["profile-pic"]}
-            />
-          </div>
-        </div>
-
-        <h1 className={styles.title}>Congratulations!</h1>
-
-        <p className={styles.subtitle}>
-          your meeting has been scheduled with one of your experts from Investza
-          <span className={styles["loading-dots"]}>
-            <span>.</span>
-            <span>.</span>
-            <span>.</span>
-          </span>
-        </p>
+    <div className={styles.loadingWrapper}>
+      <div className={styles.skCubeGrid}>
+        <div className={`${styles.skCube} ${styles.skCube1}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube2}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube3}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube4}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube5}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube6}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube7}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube8}`}></div>
+        <div className={`${styles.skCube} ${styles.skCube9}`}></div>
       </div>
     </div>
   );
